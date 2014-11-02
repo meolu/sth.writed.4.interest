56981147238be59300799f903a9b45fac9328f0d
meolu <861459320@qq.com>
2014-11-02 15:55:30 +0800

    ip2city

---
+++ ip2city/example/test-IP2CITY.class.php
@@ -0,0 +1,13 @@
+<?php
+/* *************************************************************************
+ * File Name: example/test-IP2CITY.class.php
+ * Author: wushuiyong
+ * mail: wushuiyong@huamanshu.com
+ * Created Time: Tue 21 Oct 2014 03:51:00 PM
+ * ************************************************************************/
+
+include dirname(__FILE__) . '/../IP2CITY.class.php';
+$ip2city = new IP2CITY("../UTFWry.dat");
+$ip = $argv[1] ? $argv[1] : "121.199.24.143";
+$address = $ip2city->ip2city($ip);
+echo $address . PHP_EOL;
